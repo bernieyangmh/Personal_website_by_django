1a7f84fa094c9fa8cdf0b9796a0347d45264e6fb
bernieyangmh <berniey@163.com>
2016-09-19 23:42:19 +0800

    80 form

--- rango/urls.py
+++ rango/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import url
 from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 from django.http import HttpResponse
 def index(request):
@@ -18,3 +19,17 @@ def show_category(request, category_name_slug):
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
