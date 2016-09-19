c1c2f76a1ede8ac338e797e9cc774328eecb8e20
bernieyangmh <berniey@163.com>
2016-09-19 22:28:47 +0800

    add and register category and page

--- populate_rango.py
+++ populate_rango.py
@@ -53,7 +53,7 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
     c.save()
     return c
 # Start execution here!
