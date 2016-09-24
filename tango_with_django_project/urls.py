a5c43b6a784fb31c8bbd9946cb8c6e6a37e4825a
bernieyangmh <berniey@163.com>
2016-09-24 10:07:17 +0800

    11 finished

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from rango import views
 
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
@@ -9,5 +10,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
-    url(r'^rango/', include('rango.urls'))
+    url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
