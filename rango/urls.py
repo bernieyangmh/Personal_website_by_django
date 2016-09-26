54968b36fce9fefdf0be70c55b7177fae919e63b
bernieyangmh <berniey@163.com>
2016-09-26 22:17:06 +0800

    15

--- rango/urls.py
+++ rango/urls.py
@@ -17,4 +17,7 @@ urlpatterns = [
     url(r'^login/$', views.user_login, name='login'),
     url(r'^restricted/', views.restricted, name='restricted'),
     url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^register_profile/$', views.register_profile, name='register_profile'),
+    url(r'^profile/(?P<username>[\w\-]+)/$', views.profile, name='profile'),
+    url(r'^profiles/$', views.list_profiles, name='list_profiles'),
 ]
